00211b1977192405025d952bd0a49a4fa26d01ea
tomasz78285 <62183978+tomasz78285@users.noreply.github.com>
2021-03-18 12:05:18 +0100

    Update udostepnianie.php

--- udostepnianie.php
+++ udostepnianie.php
@@ -32,12 +32,62 @@
 
                         <div class="col-sm-4" id="s4">
                             <h1>Pobierz link</h1>
+                            <a href="http://localhost/System_ankietyzacji_online/przykladowe_ankiety.php">http://localhost/System_ankietyzacji_online/przykladowe_ankiety.php</a>
                         </div>
+                        <form method="post">
+                            <div class="col-sm-4" id="s5">
+                                <h1>Wyślij zaproszenie e-mail</h1>
+                                <label>Wpisz adresata</label>
+                                <input type="text" name="adresat" />
+                                <input type="submit" value="Wyślij" />
+                            </div>
+                        </form>
+                        <?php
+                            include_once "PHPMailer/PHPMailer.php";
+                            include_once "PHPMailer/Exception.php";
+                            //Import PHPMailer classes into the global namespace
+                            //These must be at the top of your script, not inside a function
+                            use PHPMailer\PHPMailer\PHPMailer;
+                            use PHPMailer\PHPMailer\SMTP;
+                            use PHPMailer\PHPMailer\Exception;
+                           
+                           
+                            //Load Composer's autoloader
+                //require 'vendor/autoload.php';
 
-                        <div class="col-sm-4" id="s5">
-                            <h1>Wyślij zaproszenie e-mail</h1>
-                        </div>
+                            //Instantiation and passing `true` enables exceptions
+                            $mail = new PHPMailer(true);
 
+                            try {
+                                //Server settings
+                                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+                                $mail->isSMTP();                                            //Send using SMTP
+                                $mail->Host       = 'ssl0.ovh.net';                     //Set the SMTP server to send through
+                                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                                $mail->Username   = 'kad.tom@wp.pl';                     //SMTP username
+                                $mail->Password   = '784-Tit.';                               //SMTP password
+                                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                                $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+                                //Recipients
+                                $mail->setFrom('kad.tom@wp.pl', 'Mailer');
+                                $mail->addAddress('tkadlubowski78285');     //Add a recipient
+                       //Name is optional
+                           //
+                               //Optional name
+
+                                //Content
+                                $mail->isHTML(true);                                  //Set email format to HTML
+                                $mail->Subject = 'Here is the subject';
+                                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                               // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                                $mail->send();
+                                echo 'Message has been sent';
+                            } catch (Exception $e) {
+                                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                            }
+                        ?>
                     </div>
                 </div>
             </section> 
